b20a4da732860969841e17a71ed1d706a7665793
Joshna M (Research Engineer) <joshnam@nousinfo.com>
2026-04-22 11:22:41 +0530

    remove node_modules and dist from git

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".",
-  build: {
-    outDir: "dist"
-  }
+  base: "/SLAM_BOOK/",
+  build: { outDir: "dist" }
 });
