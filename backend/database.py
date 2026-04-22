00899e80be5dc9388da3fe1bec71c85157cfbd3e
Joshna M (Research Engineer) <joshnam@nousinfo.com>
2026-04-22 15:12:21 +0530

    fix mysql url format

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,14 @@ from sqlalchemy.orm import sessionmaker
 from structure import Base
 import os
 
-MYSQL_URL = os.getenv("MYSQL_URL", "mysql+pymysql://root:RTEdEzgasJBbNmfniMRDHhpIrWZKWQSS@shinkansen.proxy.rlwy.net:54209/railway")
+MYSQL_URL = os.environ.get(
+    "MYSQL_URL",
+    "mysql+pymysql://root:RTEdEzgasJBbNmfniMRDHhpIrWZKWQSS@shinkansen.proxy.rlwy.net:54209/railway"
+)
+
+# Replace mysql:// with mysql+pymysql://
+if MYSQL_URL.startswith("mysql://"):
+    MYSQL_URL = MYSQL_URL.replace("mysql://", "mysql+pymysql://", 1)
 
 engine = create_engine(MYSQL_URL)
 SessionLocal = sessionmaker(bind=engine)
